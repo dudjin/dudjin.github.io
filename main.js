80257b33f1ce10ccdfbcd8a10cffc4a3bb3a595a
dudjin <82287214+dudjin@users.noreply.github.com>
2021-05-10 09:42:34 +0900

    reopen

---
+++ main.js
@@ -0,0 +1,86 @@
+'use strict';
+
+
+const home = document.querySelector('#home');
+const homeContainer = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+
+
+// Make navbar transparent when it is on the top
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    //console.log(windown.scrollY);
+    //console.log(`navbarHeight: ${navbarHeight}`);
+    if(window.scrollY > navbarHeight) {
+        navbar.classList.add('navbar--dark');
+        navbar.classList.add('.navbar_menu_item.active--dark');
+    } else {
+        navbar.classList.remove('navbar--dark');
+        navbar.classList.remove('.navbar_menu_item.active--dark');
+    }
+});
+
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar_menu');
+navbarMenu.addEventListener('click', (event) => {
+    //console.log('dsf');
+    //console.log(event.target);
+    //console.log(event.target.dataset.link);
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+});
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    const rect = scrollTo.getBoundingClientRect();
+    window.scrollTo(0, rect.top);
+}
+
+//페이지이동 : newPage1()
+function mainPage() {
+    window.location.href = 'index.html';
+}
+
+function newPage1() {
+    window.location.href = 'sub1.html';
+}
+
+//Handle click on "contact me" button on home
+const homeContactBtn = document.querySelector('.home_contact');
+homeContactBtn.addEventListener('click', () => {
+    const scrollTo = document.querySelector('#contact');
+    scrollTo.scrollIntoView({behavior: "smooth"});
+});
+
+
+
+
+// Navbar toggle button for small screen 
+const navbarToggleBtn = document.querySelector('.navbar_toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
+});
+
+
+//Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', ()=> {
+    if(window.scrollY > homeHeight /2) {
+        arrowUp.classList.add('visible');
+    } else {
+        arrowUp.classList.remove('visible');
+    }
+});
+
+// Handle click on the "arrow up" button
+arrowUp.addEventListener('click', () => {
+    scrollIntoView('#home');
+});
